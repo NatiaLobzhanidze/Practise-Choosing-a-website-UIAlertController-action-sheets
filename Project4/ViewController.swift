7d84b723866ce6e2b12fcf95b6c4687251abaddb
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-03-14 20:57:13 +0400

    add ProgressBar and refresh button

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, WKNavigationDelegate{
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -19,13 +21,26 @@ class ViewController: UIViewController, WKNavigationDelegate{
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "open", style: .plain, target: self, action: #selector(tappedBtn))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressBtn = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressBtn,spacer,refresh]
+        navigationController?.isToolbarHidden = false
+        
+       
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url:url))
         webView.allowsBackForwardNavigationGestures = true
     }
     @objc func tappedBtn(){
         let ac = UIAlertController(title: "Open page ", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com ", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "adjaranet.com", style: .default, handler: openPage))
         ac.addAction(UIAlertAction(title: "w3schools.com", style: .destructive, handler: openPage))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
@@ -39,6 +54,11 @@ class ViewController: UIViewController, WKNavigationDelegate{
     }
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 }
 
